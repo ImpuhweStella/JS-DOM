7fe849fb23a0741968ee5acc94a7f42df72271a2
ImpuhweStella <stellahimpuhwe346@gmail.com>
2022-10-19 02:25:18 +0200

    JS-DOM Changes

---
+++ ass.js
@@ -0,0 +1,11 @@
+function subscribe(){
+    var username = document.getElementById("username");
+    var mail=document.getElementById("mail");
+    var field1= document.getElementById("field1");
+    var field2= document.getElementById("field2");
+    field1.innerHTML="Please enter your name";
+    field2.innerHTML="Please enter your Email";
+    // document.getElementsByClassName(".inputs input").style.color= "red";
+    // var x= document.querySelectorAll(".in")
+}
+
